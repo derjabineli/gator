81e349a6f5b83a5ce3eb68d8ea7121d04d43b25a
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-01-10 14:31:33 -0800

    separate handler and commands logic

--- commands.go
+++ commands.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
 
 	"github.com/derjabineli/gator/internal/config"
 	"github.com/derjabineli/gator/internal/database"
-	"github.com/google/uuid"
 )
 
 type state struct {
@@ -38,42 +35,5 @@ func (c *commands) run(s *state, cmd command) error {
 	if err != nil {
 		return err
 	}
-	return nil
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) < 3 {
-		return fmt.Errorf("you must provide a username")
-	}
-
-	userName := cmd.args[2]
-
-	user, err := s.db.GetUser(context.Background(), userName)
-	if err != nil {
-		return fmt.Errorf("username not found")
-	}
-
-	err = s.cfg.SetUser(user.Name)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("The User has successfully been set to %s\n", userName)
-	return nil
-}
-
-func handlerRegister(s *state, cmd command) error {
-	if len(cmd.args) < 3 {
-		return fmt.Errorf("you must provide a username")
-	}
-
-	userName := cmd.args[2]
-
-	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: userName})
-	if err != nil {
-		return fmt.Errorf("username not available")
-	}
-
-	s.cfg.SetUser(user.Name)
 	return nil
 }
