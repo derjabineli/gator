a09f6e640f924aafaef88a0caf791cb77e278330
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-01-09 19:13:42 -0800

    adjust main to utilize commands set up

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/derjabineli/gator/internal/config"
 )
@@ -11,14 +12,28 @@ func main() {
 	if err != nil {
 		fmt.Printf("error reading config. error: %v\n", err)
 	}
-	
-	err = cfg.SetUser("eli")
-	if err != nil {
-		fmt.Printf("error setting user. error: %v\n", err)
+
+	state := State{config: &cfg}
+	commands := Commands{
+		cmds: make(map[string]func(*State, Command) error),
 	}
-	
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Printf("error reading config. error: %v\n", err)
+
+	commands.register("login", handlerLogin)
+
+	args := os.Args
+
+	if len(args) == 1 {
+		fmt.Println("error: not enough arguments provided") 
+		os.Exit(1)
 	}
+
+	cmd := Command{name: args[1], args: args}
+
+	err = commands.run(&state, cmd)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
