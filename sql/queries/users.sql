93dc7dd5139c8240a39566ce8c623023ffa11fe0
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-01-15 11:50:31 -0800

    add get user by id query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -8,9 +8,13 @@ VALUES (
 )
 RETURNING *;
 
--- name: GetUser :one
+-- name: GetUserByName :one
 SELECT * FROM users 
 WHERE users.name = $1;
+
+-- name: GetUserByID :one
+SELECT * FROM users 
+WHERE users.id = $1;
 
 -- name: ResetUsers :exec
 DELETE FROM users;
