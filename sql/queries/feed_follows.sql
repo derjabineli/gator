d11bb8f0c7e678e846cff60f4846f638f4b5392f
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-01-15 13:08:23 -0800

    add query to remove feed follows

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -18,3 +18,7 @@ SELECT feed_follows.*, feeds.name FROM feed_follows
 LEFT JOIN feeds 
 ON feed_follows.feed_id = feeds.id
 WHERE feed_follows.user_id = $1;
+
+-- name: UnfollowFeed :exec
+DELETE FROM feed_follows
+WHERE feed_follows.user_id = $1 AND feed_follows.feed_id = $2;
