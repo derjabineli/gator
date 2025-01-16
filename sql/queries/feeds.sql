859831e190da201511246b67ea3e9aa91fd6d1e8
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-01-15 17:27:00 -0800

    add queries to retrieve feeds to fetch and mark them as fetched

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -16,3 +16,19 @@ SELECT * FROM feeds;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2,
+    updated_at = $3
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT feed_follows.feed_id, feeds.*
+FROM feed_follows
+RIGHT JOIN feeds 
+ON feed_follows.feed_id = feeds.id
+WHERE feed_follows.user_id = $1
+ORDER BY feeds.last_fetched_at ASC
+NULLS FIRST
+LIMIT 1;
